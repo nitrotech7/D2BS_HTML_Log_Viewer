78cfc42e4d0979e4e86c0ad56c8cc7836481a736
nitrotech7 <53409493+nitrotech7@users.noreply.github.com>
2020-02-01 20:23:22 +0100

    Add files via upload

---
+++ D2BS_HTML_Log_Viewer_Parses_Last_1000_Lines.bat
@@ -0,0 +1,42 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+REM You always have to use SETLOCAL ENABLEDELAYEDEXPANSION and !...! instead of %...% when working with variables which are modified inside a loop.
+
+set "_file=..\d2bs.log"
+
+if exist !_file! (
+
+	for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c_%%a_%%b)
+	for /f "tokens=1-3 delims=/:" %%a in ("%TIME: =0%") do (set mytime=%%a_%%b_%%c)
+	set "_datetime=!mydate!_!mytime!"
+	REM echo !_datetime!
+	
+	:: Parse Method 1: parse last X lines
+	set /a _lastXLines=1000
+	set "_lines=last_!_lastXLines!"
+	bin\tail -n !_lastXLines! !_file! > d2bs_part.log
+
+	
+	:: Add line numbers to file  http://tuxthink.blogspot.com/2012/01/adding-line-numbers-to-file.html
+	bin\sed "/./=" d2bs_part.log | bin\sed "/./N; s/\n/ /" > d2bs_part2.log
+
+	:: Regex the file for proper html output (ie. converting game color codes to CSS classes)
+	bin\sed -E -f regex.sh d2bs_part2.log > d2bs_edited.html
+
+	:: Attach the header and footer htmls to our html log file
+	type _header.html d2bs_edited.html _footer.html > logs\d2bs_lines_!_lines!_!_datetime!.html
+
+	:: Clean up junk files
+	del d2bs_part.log d2bs_part2.log d2bs_edited.html
+
+	:: Launch parse log html in Chrome browser
+	start chrome logs\d2bs_lines_!_lines!_!_datetime!.html
+) else (
+   echo Couldnt find d2bs.log. Make sure you placed this folder in your d2bs folder!
+)
+
+timeout 2
+REM pause
+
+
---
+++ D2BotManagerConsole_Viewer_Last_1000.bat
@@ -0,0 +1,44 @@
+@echo off
+
+setlocal EnableDelayedExpansion
+REM You always have to use SETLOCAL ENABLEDELAYEDEXPANSION and !...! instead of %...% when working with variables which are modified inside a loop.
+
+REM I:\Games\D2_kolbot_Dec_2019\trunk\D2BotManagerConsole\kolbot\logs
+
+set "_file=..\kolbot\logs\D2BotManagerConsole.log"
+
+
+if exist !_file! (
+
+	for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c_%%a_%%b)
+	for /f "tokens=1-3 delims=/:" %%a in ("%TIME: =0%") do (set mytime=%%a_%%b_%%c)
+	set "_datetime=!mydate!_!mytime!"
+	REM echo !_datetime!
+	
+	:: Parse Method 1: parse last X lines
+	set /a _lastXLines=1000
+	set "_lines=last_!_lastXLines!"
+	bin\tail -n !_lastXLines! !_file! > D2BotManagerConsole_part.log
+	
+	:: Add line numbers to file  http://tuxthink.blogspot.com/2012/01/adding-line-numbers-to-file.html
+	bin\sed "/./=" D2BotManagerConsole_part.log | bin\sed "/./N; s/\n/) /" > D2BotManagerConsole_part2.log
+
+	:: Regex the file for proper html output (ie. converting game color codes to CSS classes)
+	bin\sed -E -f regex.sh D2BotManagerConsole_part2.log > D2BotManagerConsole_edited.html
+
+	:: Attach the header and footer htmls to our html log file
+	type _header.html D2BotManagerConsole_edited.html _footer.html > logs\D2BotManagerConsole_lines_!_lines!_!_datetime!.html
+
+	:: Clean up junk files
+	del D2BotManagerConsole_part.log D2BotManagerConsole_part2.log D2BotManagerConsole_edited.html
+
+	:: Launch parse log html in Chrome browser
+	start chrome logs\D2BotManagerConsole_lines_!_lines!_!_datetime!.html
+) else (
+   echo Couldnt find D2BotManagerConsole.log. Make sure you placed this folder in your D2BotManagerConsole folder!
+)
+
+timeout 2
+REM pause
+
+
